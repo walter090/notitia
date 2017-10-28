4ef391caa903c616bc65312922b9531db032ce4a
walter_wu090 <walter.wu090@gmail.com>
2017-10-28 00:50:22 -0400

    Add js for handling ajax form submission

---
+++ static/js/post.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('#join-list').submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/join/',
+            data: {
+                csrfmiddlewaretoken: Cookies.get('csrftoken'),
+                email: $('#email').val()
+            },
+
+            success: function () {
+                $('#email').val('');
+                document.getElementById('message').innerHTML =
+                    'You are now in the mailing list, thank you for your interest!';
+            },
+            error: function () {
+                document.getElementById('message').innerHTML =
+                    'Sorry, we have encountered an error, maybe try again later?';
+            }
+        });
+    });
+});
+
