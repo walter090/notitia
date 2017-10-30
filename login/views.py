78ae932905622400587da3fc440925726f87d943
walter_wu090 <walter.wu090@gmail.com>
2017-10-30 01:21:49 -0400

    Update login view
    I am changing all views to class based.

--- login/views.py
+++ login/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.views.generic import FormView
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse('login')
+class LoginView(FormView):
+    template_name = 'login/login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        raise NotImplementedError
+
+
+class SignUpView(FormView):
+    template_name = 'login/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        raise NotImplementedError
