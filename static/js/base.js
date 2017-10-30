81e155a44fdbedb580e45422726c926de00918f8
walter_wu090 <walter.wu090@gmail.com>
2017-10-30 01:19:48 -0400

    Update static files

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    $('#hamburger').on('click', function () {
+    $('#hamburger').on('click', function (event) {
         document.getElementById('hamburger').classList.toggle('cross');
+        document.getElementById('menu').classList.toggle('expose');
+        document.getElementById('content').classList.toggle('shimmy');
+        event.stopPropagation();
     });
+
+    $('#content').on('click', function (event) {
+        $('#hamburger').removeClass('cross');
+        $('#menu').removeClass('expose');
+        $('#content').removeClass('shimmy');
+        event.stopPropagation();
+    })
 });
