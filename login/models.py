80aa261c5a6221c0de5b3e378cc8783aba187faa
walter_wu090 <walter.wu090@gmail.com>
2017-10-30 23:33:39 -0400

    Add member since field to User model

--- login/models.py
+++ login/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.core import validators
 from notitia import settings
+from django.contrib.auth import hashers
 
 
 class User(models.Model):
     email = models.CharField(max_length=100,
                              validators=[validators.EmailValidator],
+                             null=False,
                              primary_key=True)
     password = models.CharField(max_length=100,
-                                validators=settings.AUTH_PASSWORD_VALIDATORS)
+                                null=False,
+                                validators=[settings.AUTH_PASSWORD_VALIDATORS])
+    member_since = models.DateField('date joined', null=True)
 
     def __str__(self):
         return self.email
