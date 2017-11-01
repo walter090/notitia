442913aa62c4e8db640cea0b456160d7b25a5b3f
walter_wu090 <walter.wu090@gmail.com>
2017-11-01 02:04:41 -0400

    Add form submission

--- login/views.py
+++ login/views.py
@@ -1,5 +1,11 @@
+import datetime
+import json
+
 from django.views.generic import FormView
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from . import models, forms
+from django.contrib.auth import hashers
 
 
 class LoginView(FormView):
@@ -12,8 +18,33 @@ class LoginView(FormView):
         raise NotImplementedError
 
 
-class SignUpView(FormView):
+class SignupView(FormView):
     template_name = 'login/signup.html'
 
     def get(self, request, *args, **kwargs):
-        raise NotImplementedError
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        signup_form = forms.SignupForm(request.POST)
+
+        if signup_form.is_valid():
+            data = signup_form.cleaned_data
+            user = models.User()
+            user.email = data['email']
+            user.password = hashers.make_password(data['password'])
+            user.member_since = datetime.date.today()
+            user.save()
+
+            return HttpResponseRedirect('/login/')
+
+        errors_dict = json.loads(signup_form.errors.as_json())
+        error_message = ''
+        print(errors_dict)
+
+        for error_type in errors_dict:
+            for error in errors_dict[error_type]:
+                error_message += error['message']
+
+        return render(request, self.template_name, context={
+            'submitted': error_message,
+        })
