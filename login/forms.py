ed4606862b0874ad9b33dd70145828b2cea19385
walter_wu090 <walter.wu090@gmail.com>
2017-11-01 00:18:51 -0400

    Update signup form

--- login/forms.py
+++ login/forms.py
@@ -1,19 +1,10 @@
-from .models import User
 from django import forms
+from django.core import validators
+from notitia import settings
 
 
-class SignupForm(forms.ModelForm):
-    password = forms.CharField(max_length=100, widget=forms.PasswordInput())
-    confirm_password = forms.CharField()
-
-    class Meta:
-        model = User
-        fields = ['email', 'password', 'confirm_password']
-
-    def clean(self):
-        cleaned_data = super(SignupForm, self).clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        if password != confirm_password:
-            raise forms.ValidationError('The passwords don\'t match!')
+class SignupForm(forms.Form):
+    email = forms.EmailField(max_length=200,
+                             validators=[validators.EmailValidator])
+    password = forms.CharField(max_length=200)
+    confirm_password = forms.CharField(max_length=200)
