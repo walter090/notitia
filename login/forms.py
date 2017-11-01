05585b88aa052bda271051e8af24d739a9c9066e
walter_wu090 <walter.wu090@gmail.com>
2017-11-01 02:03:33 -0400

    Update register form
    Add custom validation error messages. Remove confirm password field, since password confirmation validation is done on client side with javascript.

--- login/forms.py
+++ login/forms.py
@@ -1,10 +1,28 @@
 from django import forms
 from django.core import validators
-from notitia import settings
+from .models import User
 
 
-class SignupForm(forms.Form):
+class SignupForm(forms.ModelForm):
     email = forms.EmailField(max_length=200,
-                             validators=[validators.EmailValidator])
+                             validators=[validators.EmailValidator],
+                             error_messages={
+                                 'invalid': 'Please enter a valid email address.\n',
+                                 'unique': 'User with this email already exists.\n',
+                             })
     password = forms.CharField(max_length=200)
-    confirm_password = forms.CharField(max_length=200)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    # def clean(self):
+    #     # Call super clean
+    #     super(SignupForm, self).clean()
+    #
+    #     user_email = self.cleaned_data['email']
+    #     try:
+    #         User.objects.get(email=user_email)
+    #         raise forms.ValidationError('{} is already registered'.format(user_email))
+    #     except User.DoesNotExist:
+    #         pass
