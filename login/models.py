5d09ae18be21813accac7dfc90966bf411b40d8b
walter_wu090 <walter.wu090@gmail.com>
2017-11-01 17:24:21 -0400

    User model now inherits AbstractBaseUser

--- login/models.py
+++ login/models.py
@@ -1,18 +1,56 @@
 from django.db import models
 from django.core import validators
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import UserManager
 
 
-class User(models.Model):
-    email = models.CharField(max_length=200,
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.CharField(_('email'),
+                             max_length=200,
                              validators=[validators.EmailValidator],
                              null=False,
                              blank=False,
                              primary_key=True,
                              unique=True)
-    password = models.CharField(max_length=200,
+    password = models.CharField(_('password'),
+                                max_length=200,
                                 null=False,
                                 blank=False)
-    member_since = models.DateField('date joined', null=True)
+    first_name = models.CharField(_('first_name'),
+                                  max_length=200,
+                                  blank=True)
+    last_name = models.CharField(_('last name'),
+                                 max_length=200,
+                                 blank=True)
+    member_since = models.DateField(_('date joined'), null=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        full_name = '{0} {1}'.format(self.first_name, self.last_name)
+        return full_name
+
+    def get_short_name(self):
+        try:
+            short = self.first_name[0].upper() + self.last_name[0].upper()
+        except IndexError:
+            return ''
+        return short
 
     def __str__(self):
         return self.email
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
