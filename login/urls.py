aaaee8bbcd67917588ccedde51584b131019f6d4
walter_wu090 <walter.wu090@gmail.com>
2017-11-02 00:29:22 -0400

    Add url routing for signup

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.LoginView.as_view(), name='login')
+    url(r'^$', views.LoginView.as_view(), name='login'),
+    url(r'^signup/', views.SignupView.as_view(), name='signup'),
 ]
