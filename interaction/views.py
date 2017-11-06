599c998c73a9d358b60c522ba429166b354c8b00
walter_wu090 <walter.wu090@gmail.com>
2017-11-06 01:15:12 -0500

    Update interaction views

--- interaction/views.py
+++ interaction/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic import FormView
+
+from . import models
+
+
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
+class MakePostView(FormView):
+    template_name = 'interaction/make_post.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={
+            'section_name': 'Draft',
+        })
+
+    def post(self, request, *args, **kwargs):
+        raise NotImplementedError
