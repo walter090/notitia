64cca3f8755c5af5f567ae18152a257409f8c193
walter_wu090 <walter.wu090@gmail.com>
2017-11-06 01:15:32 -0500

    Update urlConf for login

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.LoginView.as_view(), name='login'),
-    url(r'^signup/', views.SignupView.as_view(), name='signup'),
+    url(r'^signup/$', views.SignupView.as_view(), name='signup'),
 ]
