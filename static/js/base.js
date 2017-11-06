5c2c5d459adfe24a6a3656724200b56a25cf881a
walter_wu090 <walter.wu090@gmail.com>
2017-11-06 01:16:26 -0500

    Update static files for base template

--- static/js/base.js
+++ static/js/base.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
         document.getElementById('hamburger').classList.toggle('cross');
         document.getElementById('menu').classList.toggle('expose');
         document.getElementById('content').classList.toggle('shimmy');
-        document.getElementById('content').classList.toggle('diable_all');
+        document.getElementById('content').classList.toggle('disable_all');
+        $('#quick_login').attr('disabled', '');
         event.stopPropagation();
     });
 
     $('#content').on('click', function (event) {
+        var content = $('#content');
         $('#hamburger').removeClass('cross');
         $('#menu').removeClass('expose');
-        $('#content').removeClass('shimmy');
-        $('#content').removeClass('disable_all');
+        content.removeClass('shimmy');
+        content.removeClass('disable_all');
+        $('#quick_login').removeAttr('disabled');
         event.stopPropagation();
     })
 });
