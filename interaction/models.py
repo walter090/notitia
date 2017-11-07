fd78415af1028416f6302c03dcdc92f839e74353
walter_wu090 <walter.wu090@gmail.com>
2017-11-06 20:52:45 -0500

    Save model after setting slug

--- interaction/models.py
+++ interaction/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
@@ -8,6 +9,7 @@ from login.models import User
 
 class Post(models.Model):
     # Use default id as primary key
+    id = models.AutoField(primary_key=True)
     created_by = models.ForeignKey(User,
                                    null=True)
     publish_time = models.DateTimeField(_('publish time'),
@@ -15,8 +17,9 @@ class Post(models.Model):
     last_modified_time = models.DateTimeField(_('last modified time'),
                                               null=True,
                                               blank=True)
-    title = models.CharField(_('title'), max_length=200, blank=False)
-    subtitle = models.CharField(_('subtitle'), max_length=400, blank=True)
+    title = models.TextField(_('title'), blank=False)
+    slug = models.SlugField(_('slug'), unique=True, null=True, blank=True)
+    subtitle = models.TextField(_('subtitle'), blank=True)
     content_body = models.TextField(_('content'), blank=False, null=False)
 
     def __str__(self):
@@ -24,7 +27,7 @@ class Post(models.Model):
 
     def make_new_post(self, request, title, content_body):
         self.created_by = request.user
-        self.title = title
+        self.title = title.strip()  # Remove leading and trailing spaces
         self.content_body = content_body
         self.last_modified_time = self.publish_time
 
@@ -38,4 +41,6 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         self.full_clean()
+        super(Post, self).save(*args, **kwargs)
+        self.slug = '{0}/{1}'.format(self.id, slugify(self.title))
         super(Post, self).save(*args, **kwargs)
