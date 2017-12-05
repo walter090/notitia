29c812ab29b56b55c28353e60113e779d3cc20d9
walter_wu090 <walter.wu090@gmail.com>
2017-12-04 23:46:46 -0500

    Add logout url

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('', views.AccountView.as_view(), name='account'),
 ]
